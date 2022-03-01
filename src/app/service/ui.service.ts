e7814cc8a11238a7b27e3108fc0dc7ed0d2f5669
JJRoma <jj_roma@yahoo.ccom.ar>
2022-03-01 16:16:20 -0300

    first commit

---
+++ src/app/service/ui.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import{ Observable, Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UiService {
+  private showAddTask:boolean=false;
+  private subjet = new Subject<any>();
+
+  constructor() { }
+  onToggle():Observable<any>{
+    return this.subjet.asObservable();
+  }
+  
+  toggleAddTask(): void{
+    this.showAddTask = !this.showAddTask;
+    this.subjet.next(this.showAddTask);
+  }
+
+
+
+}
